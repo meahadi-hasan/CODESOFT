c2cc609639977c24d49bcbac85f141a954dd4935
Md. Meahadi Hasan <139465566+meahadi-hasan@users.noreply.github.com>
2025-02-02 01:26:59 +0600

    Add files via upload

---
+++ Landing Page/script.js
@@ -0,0 +1,12 @@
+
+document.querySelector('.buy-now').addEventListener('click', function() {
+    alert('Thank you for your interest!');
+});
+
+document.querySelector('.create-account').addEventListener('click', function() {
+    alert('Create an Account clicked!');
+});
+
+document.querySelector('.search-icon').addEventListener('click', function() {
+    alert('Search clicked!');
+});
